92171b6315a4a757da4ef9f8e48e0106dfc12a15
Wilsonsoft <wherrera.data@gmail.com>
2025-03-25 18:35:04 -0500

    Update Whatsapp.jsx

--- components/social/Whatsapp.jsx
+++ components/social/Whatsapp.jsx
@@ -8,7 +8,8 @@ const Whatsapp = () => {
             <p>Chatea con nosotros</p>
         </div>
         <button>
-            <a href="https://wa.me/573134229281" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/573134229281" text="Bienvenido%20a%20ColombiaGas.%20En%20que%20podemos%20ayudarle" 
+		target="_blank" rel="noopener noreferrer">
                 <img src="/icon/whatsapp.svg" alt="icono de contacto por whatsapp" />
             </a>
 
